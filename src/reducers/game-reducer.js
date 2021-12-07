5f89d0aa8b71a9e9fc881f6a477c64a8d173c8f8
xScoopy <69348168+xScoopy@users.noreply.github.com>
2021-12-06 18:49:50 -0600

    move left and right

--- src/reducers/game-reducer.js
+++ src/reducers/game-reducer.js
@@ -20,8 +20,15 @@ const gameReducer = (state = defaultState(), action) => {
       }
       return state;
     case MOVE_RIGHT:
+      if (canMoveTo(shape, grid, x + 1, y, rotation)) {
+        return { ...state, x: x + 1 };
+      }
       return state;
     case MOVE_LEFT:
+      // subtract 1 from the x and check if this new position is possible by calling `canMoveTo()
+      if (canMoveTo(shape, grid, x - 1, y, rotation)) {
+        return { ...state, x: x - 1 };
+      }
       return state;
     case MOVE_DOWN:
       return state;
