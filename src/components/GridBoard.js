4116cfc7c5ddf89290a21145707631f13c0315c8
xScoopy <69348168+xScoopy@users.noreply.github.com>
2021-12-06 19:02:10 -0600

    timer created

--- src/components/GridBoard.js
+++ src/components/GridBoard.js
@@ -1,42 +1,65 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import GridSquare from "./GridSquare";
-import { useSelector } from "react-redux";
-import { shapes } from "../utils"
+import { useSelector, useDispatch } from "react-redux";
+import { shapes } from "../utils";
+import { moveDown } from "../actions";
 
 // Represents a 10 x 18 grid of grid squares
 
 export default function GridBoard(props) {
-	const game = useSelector((state) => state.game)
-	const { grid, shape, rotation, x, y, isRunning, speed } = game
+  const requestRef = useRef();
+  const lastUpdateTimeRef = useRef(0);
+  const progressTimeRef = useRef(0);
+  const dispatch = useDispatch();
+  const game = useSelector((state) => state.game);
+  const { grid, shape, rotation, x, y, isRunning, speed } = game;
 
-	const block = shapes[shape][rotation]
-  const blockColor = shape
+  const block = shapes[shape][rotation];
+  const blockColor = shape;
   // map rows
+  useEffect(() => {
+    requestRef.current = requestAnimationFrame(update);
+    return () => cancelAnimationFrame(requestRef.current);
+  }, [isRunning]);
   const gridSquares = grid.map((rowArray, row) => {
     // map columns
     return rowArray.map((square, col) => {
       // Find the block x and y on the shape grid
       // By subtracting the x and y from the col and the row we get the position of the upper left corner of the block array as if it was superimposed over the main grid
-      const blockX = col - x
-      const blockY = row - y
-      let color = square
+      const blockX = col - x;
+      const blockY = row - y;
+      let color = square;
       // Map current falling block to grid.
       // For any squares that fall on the grid we need to look at the block array and see if there is a 1 in this case we use the block color.
-      if (blockX >= 0 && blockX < block.length && blockY >= 0 && blockY < block.length) {
-        color = block[blockY][blockX] === 0 ? color : blockColor
+      if (
+        blockX >= 0 &&
+        blockX < block.length &&
+        blockY >= 0 &&
+        blockY < block.length
+      ) {
+        color = block[blockY][blockX] === 0 ? color : blockColor;
       }
       // Generate a unique key for every block
       const k = row * grid[0].length + col;
       // Generate a grid square
-      return <GridSquare
-              key={k}
-              color={color} />
-    })
-  })
-
-	return (
-		<div className='grid-board'>
-			{gridSquares}
-		</div>
-	)
+      return <GridSquare key={k} color={color} />;
+    });
+  });
+  const update = (time) => {
+    requestRef.current = requestAnimationFrame(update);
+    if (!isRunning) {
+      return;
+    }
+    if (!lastUpdateTimeRef.current) {
+      lastUpdateTimeRef.current = time;
+    }
+    const deltaTime = time - lastUpdateTimeRef.current;
+    progressTimeRef.current += deltaTime;
+    if (progressTimeRef.current > speed) {
+      dispatch(moveDown());
+      progressTimeRef.current = 0;
+    }
+    lastUpdateTimeRef.current = time;
+  };
+  return <div className="grid-board">{gridSquares}</div>;
 }
